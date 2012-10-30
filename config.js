6d5405a51c3728d49bcdc1e39035214fe294bcf2
Ezequias Dinella <ezequias.net@gmail.com>
2012-10-30 09:46:02 -0200

    Configure with NPM

--- config.js
+++
@@ -1,9 +0,0 @@
-
-// access
-exports.port = 3000;
-
-// dirs and files
-exports.publicDir = __dirname+'/public';
-exports.uploadsDir = __dirname+'/uploads';
-exports.libraryDir = exports.publicDir+'/library';
-exports.libraryFile = exports.libraryDir+'/library.json';
